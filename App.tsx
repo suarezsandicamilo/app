1796d0e07318e77eff643487d23356b352b8aa71
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-01-31 14:03:16 -0600

    Add the home screen to the app component

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { HomeScreen } from './src/screens/home_screen';
+
 import { PathScreen } from './src/screens/path_screen';
 
 const Stack = createNativeStackNavigator();
@@ -12,6 +14,13 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        ></Stack.Screen>
         <Stack.Screen
           name="Path"
           component={PathScreen}
