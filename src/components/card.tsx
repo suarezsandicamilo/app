27cd86a1e2a844ff88fa255e45bca9baa6d94114
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-02-01 12:44:44 -0600

    Create card component

---
+++ src/components/card.tsx
@@ -0,0 +1,36 @@
+//
+
+import {
+  Appearance,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from 'react-native';
+
+import colors from '../colors.json';
+
+const scheme = Appearance.getColorScheme();
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors[scheme].body_bg,
+    borderRadius: 2,
+    borderWidth: 0,
+    color: colors[scheme].body_color,
+    elevation: 2,
+    margin: 10,
+    padding: 20,
+  },
+});
+
+type Props = {
+  children?: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+};
+
+const Card = (props: Props) => {
+  return <View style={styles.container}>{props.children}</View>;
+};
+
+export { Card };
