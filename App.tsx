dd66227a4760e41bb5e7f3c35c49bfda3bd0f04f
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-02-15 14:53:00 -0600

    Create the screens for create section, create lesson and create task

--- App.tsx
+++ App.tsx
@@ -10,9 +10,15 @@ import { HomeScreen } from './src/screens/home_screen';
 
 import { PathScreen } from './src/screens/path_screen';
 
+import { SectionScreen } from './src/screens/section_screen';
+
 import { AdminScreen } from './src/screens/admin_screen';
 
-import { SectionScreen } from './src/screens/section_screen';
+import { CreateSectionScreen } from './src/screens/create_section_screen';
+
+import { CreateLessonScreen } from './src/screens/create_lesson_screen';
+
+import { CreateTaskScreen } from './src/screens/create_task_screen';
 
 import { DataController } from './src/controllers/data_controller';
 
@@ -24,6 +30,7 @@ const App = () => {
       // NOTE: IF DEVELOPMENT
       await DataController.clear('sections');
 
+      // NOTE: IF DEVELOPMENT
       await DataController.start(true);
     })();
   });
@@ -55,6 +62,27 @@ const App = () => {
         <Stack.Screen
           name="Section"
           component={SectionScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="CreateSection"
+          component={CreateSectionScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="CreateLesson"
+          component={CreateLessonScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="CreateTask"
+          component={CreateTaskScreen}
           options={{
             headerShown: false,
           }}
---
+++ src/screens/create_lesson_screen.tsx
@@ -0,0 +1,81 @@
+//
+
+import { useState } from 'react';
+
+import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+
+import { Header, Icon } from '@rneui/base';
+
+import { ThemeContext, getColor } from '../colors';
+
+const CreateLessonScreen = ({ navigation }: any) => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: getColor(theme, 'body_bg'),
+      flex: 1,
+    },
+    header: {
+      backgroundColor: getColor(theme, 'primary'),
+      borderColor: getColor(theme, 'primary'),
+      height: 80,
+      justifyContent: 'center',
+    },
+    headerIcon: {
+      height: '100%',
+      justifyContent: 'center',
+    },
+    headerText: {
+      color: getColor(theme, 'white'),
+      fontSize: 20,
+    },
+  });
+
+  const headerIcon = (
+    <Icon
+      name="arrow-back"
+      color={getColor(theme, 'white')}
+      style={styles.headerIcon}
+      onPress={() => {
+        navigation.navigate('Admin');
+      }}
+    />
+  );
+
+  const rightIcon = (
+    <Icon
+      name="star"
+      color={getColor(theme, 'white')}
+      style={styles.headerIcon}
+      onPress={() => {
+        if (theme === 'light') {
+          setTheme('dark');
+        } else {
+          setTheme('light');
+        }
+      }}
+    />
+  );
+
+  const headerText = <Text style={styles.headerText}>Aplicación</Text>;
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar backgroundColor={getColor(theme, 'primary')} />
+        <Header
+          containerStyle={styles.header}
+          statusBarProps={{
+            backgroundColor: getColor(theme, 'primary'),
+          }}
+          leftComponent={headerIcon}
+          centerComponent={headerText}
+          rightComponent={rightIcon}
+        />
+      </SafeAreaView>
+    </ThemeContext.Provider>
+  );
+};
+
+export { CreateLessonScreen };
