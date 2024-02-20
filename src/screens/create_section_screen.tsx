c7d05a925aff47049d2ff18c4114ac59563885d1
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-02-20 15:01:35 -0600

    Add form control and styles to the create section screen

--- src/screens/create_section_screen.tsx
+++ src/screens/create_section_screen.tsx
@@ -10,6 +10,10 @@ import {
   View,
 } from 'react-native';
 
+import { Section } from '../models/section';
+
+import { SectionsController } from '../controllers/SectionsController';
+
 import { AppButton } from '../components/app_button';
 
 import { AppHeader } from '../components/app_header';
@@ -18,6 +22,16 @@ import { ThemeContext, getColor } from '../colors';
 
 const CreateSectionScreen = ({ navigation }: any) => {
   const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  // Form Data
+  const [section, setSection] = useState<Partial<Section>>({});
+
+  const update = (key: keyof Section, value: any) => {
+    setSection((section) => ({
+      ...section,
+      [key]: value,
+    }));
+  };
 
   const styles = StyleSheet.create({
     container: {
@@ -35,7 +49,25 @@ const CreateSectionScreen = ({ navigation }: any) => {
       marginBottom: 10,
       padding: 10,
     },
+    requiredFormTextInput: {
+      borderColor: getColor(theme, 'danger'),
+      borderWidth: 1,
+    },
   });
+
+  const getTextInputStyle = (key: keyof Section) => {
+    const value = section[key];
+
+    if (typeof value === 'string') {
+      if (value.length > 0) {
+        return [styles.formTextInput];
+      }
+
+      return [styles.formTextInput, styles.requiredFormTextInput];
+    }
+
+    return [styles.formTextInput];
+  };
 
   return (
     <ThemeContext.Provider value={theme}>
@@ -44,25 +76,43 @@ const CreateSectionScreen = ({ navigation }: any) => {
         <AppHeader
           text="Crear Sección"
           leftIcon="arrow-back"
-          onLeftIconPress={() => {
-            navigation.goBack();
-          }}
-          onRightIconPress={() => {
-            setTheme(theme === 'light' ? 'dark' : 'light');
-          }}
+          onLeftIconPress={() => navigation.goBack()}
+          onRightIconPress={() =>
+            setTheme(theme === 'light' ? 'dark' : 'light')
+          }
         />
         <View style={styles.form}>
           <TextInput
-            style={styles.formTextInput}
+            style={getTextInputStyle('name')}
             placeholder="Nombre"
             placeholderTextColor={getColor(theme, 'body_color')}
+            onChangeText={(text) => update('name', text)}
           />
           <TextInput
-            style={styles.formTextInput}
+            style={getTextInputStyle('description')}
             placeholder="Descripción"
             placeholderTextColor={getColor(theme, 'body_color')}
+            onChangeText={(text) => update('description', text)}
           />
-          <AppButton text="Enviar" />
+          <AppButton
+            text="Enviar"
+            onPress={() => {
+              const name = section.name ?? '';
+              const description = section.description ?? '';
+
+              update('name', name);
+              update('description', description);
+
+              if (name.length > 0 && description.length > 0) {
+                SectionsController.add({
+                  name,
+                  description,
+                });
+
+                navigation.goBack();
+              }
+            }}
+          />
         </View>
       </SafeAreaView>
     </ThemeContext.Provider>
