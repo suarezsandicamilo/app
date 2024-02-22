11d4471aa5c399648e132cb31b8d24e52477a519
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-02-22 13:52:33 -0600

    Add form control to the create task screen

--- src/screens/create_task_screen.tsx
+++ src/screens/create_task_screen.tsx
@@ -16,17 +16,19 @@ import { Section } from '../models/section';
 
 import { Lesson } from '../models/lesson';
 
+import { Task } from '../models/task';
+
 import { SectionsController } from '../controllers/sections_controller';
 
 import { LessonsController } from '../controllers/lessons_controller';
+
+import { TasksController } from '../controllers/tasks_controller';
 
 import { AppButton } from '../components/app_button';
 
 import { AppHeader } from '../components/app_header';
 
 import { ThemeContext, getColor } from '../colors';
-
-import { firestore, db } from './../firebase';
 
 const CreateTaskScreen = ({ navigation }: any) => {
   const [theme, setTheme] = useState<'light' | 'dark'>('light');
@@ -40,6 +42,15 @@ const CreateTaskScreen = ({ navigation }: any) => {
   const [section, setSection] = useState<Section>();
 
   const [lesson, setLesson] = useState<Lesson>();
+
+  const [task, setTask] = useState<Partial<Task>>({});
+
+  const update = (key: keyof Task, value: any) => {
+    setTask((task) => ({
+      ...task,
+      [key]: value,
+    }));
+  };
 
   // Fetch Data
   useEffect(() => {
@@ -81,7 +92,25 @@ const CreateTaskScreen = ({ navigation }: any) => {
       borderWidth: 1,
       marginBottom: 10,
     },
+    requiredFormTextInput: {
+      borderColor: getColor(theme, 'danger'),
+      borderWidth: 1,
+    },
   });
+
+  const getTextInputStyle = (key: keyof Task) => {
+    const value = task[key];
+
+    if (typeof value === 'string') {
+      if (value.length > 0) {
+        return [styles.formTextInput];
+      }
+
+      return [styles.formTextInput, styles.requiredFormTextInput];
+    }
+
+    return [styles.formTextInput];
+  };
 
   return (
     <ThemeContext.Provider value={theme}>
@@ -90,12 +119,10 @@ const CreateTaskScreen = ({ navigation }: any) => {
         <AppHeader
           text="Crear Tarea"
           leftIcon="arrow-back"
-          onLeftIconPress={() => {
-            navigation.goBack();
-          }}
-          onRightIconPress={() => {
-            setTheme(theme === 'light' ? 'dark' : 'light');
-          }}
+          onLeftIconPress={() => navigation.goBack()}
+          onRightIconPress={() =>
+            setTheme(theme === 'light' ? 'dark' : 'light')
+          }
         />
         <View style={styles.form}>
           <View style={styles.pickerContainer}>
@@ -129,16 +156,36 @@ const CreateTaskScreen = ({ navigation }: any) => {
             </Picker>
           </View>
           <TextInput
-            style={styles.formTextInput}
+            style={getTextInputStyle('name')}
             placeholder="Nombre"
             placeholderTextColor={getColor(theme, 'body_color')}
+            onChangeText={(text) => update('name', text)}
           />
           <TextInput
-            style={styles.formTextInput}
+            style={getTextInputStyle('description')}
             placeholder="Descripción"
             placeholderTextColor={getColor(theme, 'body_color')}
+            onChangeText={(text) => update('description', text)}
           />
-          <AppButton text="Enviar" />
+          <AppButton
+            text="Enviar"
+            onPress={() => {
+              const name = task.name ?? '';
+              const description = task.description ?? '';
+
+              update('name', name);
+              update('description', description);
+
+              if (name.length > 0 && description.length > 0) {
+                TasksController.add(lesson, {
+                  name,
+                  description,
+                });
+
+                navigation.goBack();
+              }
+            }}
+          />
         </View>
       </SafeAreaView>
     </ThemeContext.Provider>
