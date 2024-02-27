3365b88fc92b3ad66ae3434e5a801d5168ceee3d
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-02-27 13:52:43 -0600

    Create indexes for the components, controllers, hooks, models and screens

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+//
+
+import { AppButton } from './app_button';
+
+import { AppHeader } from './app_header';
+
+import { CodeView } from './code_view';
+
+import { LessonFab } from './lesson_fab';
+
+import { SectionCard } from './section_card';
+
+export { AppButton, AppHeader, CodeView, LessonFab, SectionCard };
