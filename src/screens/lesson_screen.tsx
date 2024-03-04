91495bf710b5194d3794065ab25e3a6ee76e598f
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-03-04 14:56:42 -0600

    Add buttons to control the progress

--- src/screens/lesson_screen.tsx
+++ src/screens/lesson_screen.tsx
@@ -1,10 +1,16 @@
 //
 
-import { SafeAreaView, StatusBar } from 'react-native';
+import { useState } from 'react';
 
-import { Lesson } from '../models';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 
-import { ProgressBar } from '../components';
+import { Lesson, Task } from '../models';
+
+import { TasksController } from '../controllers';
+
+import { AppButton, ProgressBar } from '../components';
+
+import { useEffectAsync } from '../hooks';
 
 import { getColor } from '../colors';
 
@@ -14,14 +20,51 @@ type Props = {
 };
 
 const LessonScreen = ({ navigation, route }: Props) => {
+  const [progress, setProgress] = useState(1);
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   const { lesson } = route.params as {
     lesson: Lesson;
   };
 
+  const styles = StyleSheet.create({
+    buttonContainer: {
+      marginVertical: 10,
+    },
+    textContainer: {
+      alignItems: 'center',
+      marginBottom: 10,
+    },
+  });
+
+  useEffectAsync(async () => setTasks(await TasksController.allOf(lesson)), []);
+
+  let progressBar = <ProgressBar progress={0} />;
+
+  if (tasks.length > 0) {
+    progressBar = <ProgressBar progress={progress / tasks.length} />;
+  }
+
   return (
     <SafeAreaView>
       <StatusBar backgroundColor={getColor('primary')} />
-      <ProgressBar progress={0.5}/>
+      {progressBar}
+      <View style={styles.textContainer}>
+        <Text>{progress}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <AppButton
+          text=">"
+          onPress={() => setProgress((p) => Math.min(p + 1, tasks.length))}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <AppButton
+          text="<"
+          onPress={() => setProgress((p) => Math.max(p - 1, 1))}
+        />
+      </View>
     </SafeAreaView>
   );
 };
