95eed583732fe280e9082b6d492162c54becb06e
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-03-18 15:18:55 -0600

    Play the phoneme sound with a button

--- src/components/grapheme_and_phoneme_theory.tsx
+++ src/components/grapheme_and_phoneme_theory.tsx
@@ -1,18 +1,46 @@
 //
 
+import { useEffect, useState } from 'react';
+
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+import { Audio } from 'expo-av';
 
 import { MaterialIcons as Icon } from '@expo/vector-icons';
 
 import { TheoryTask } from '../models';
 
+import { getColor } from '../colors';
+
+import { sounds } from '../sounds';
+
 type Props = {
   task: TheoryTask;
 };
 
-import { getColor } from '../colors';
-
 const GraphemeAndPhonemeTheory = ({ task }: Props) => {
+  const [sound, setSound] = useState<Audio.Sound>();
+
+  const playSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(sounds[task.phoneme]);
+
+    setSound(sound);
+
+    await sound.playAsync();
+  };
+
+  useEffect(() => {
+    playSound();
+  }, []);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
   const styles = StyleSheet.create({
     button: {
       alignItems: 'center',
@@ -41,7 +69,7 @@ const GraphemeAndPhonemeTheory = ({ task }: Props) => {
       <Pressable style={styles.button}>
         <Text style={styles.grapheme}>{task.grapheme}</Text>
       </Pressable>
-      <Pressable style={styles.button}>
+      <Pressable style={styles.button} onPress={playSound}>
         <Icon name="volume-up" color={getColor('white')} size={42} />
       </Pressable>
     </View>
