247d2262bdf8146ce9ed99cdea6e87f8e84f189c
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-03-18 14:28:03 -0600

    Add buttons to the grapheme and phoneme theory component

--- src/components/grapheme_and_phoneme_theory.tsx
+++ src/components/grapheme_and_phoneme_theory.tsx
@@ -1,14 +1,51 @@
 //
 
-import { Text } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+import { MaterialIcons as Icon } from '@expo/vector-icons';
+
 import { TheoryTask } from '../models';
 
 type Props = {
   task: TheoryTask;
 };
 
+import { getColor } from '../colors';
+
 const GraphemeAndPhonemeTheory = ({ task }: Props) => {
-  return <Text>111!</Text>;
+  const styles = StyleSheet.create({
+    button: {
+      alignItems: 'center',
+      backgroundColor: getColor('primary'),
+      borderRadius: 16,
+      elevation: 4,
+      height: 100,
+      justifyContent: 'center',
+      margin: 20,
+      width: 100,
+    },
+    container: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    grapheme: {
+      color: getColor('white'),
+      fontSize: 42,
+      includeFontPadding: false,
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.button}>
+        <Text style={styles.grapheme}>{task.grapheme}</Text>
+      </Pressable>
+      <Pressable style={styles.button}>
+        <Icon name="volume-up" color={getColor('white')} size={42} />
+      </Pressable>
+    </View>
+  );
 };
 
 export { GraphemeAndPhonemeTheory };
