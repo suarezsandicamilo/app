1c4b3d2a4f626c8e18a603a80311c5c5d2fc8f31
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-03-18 13:45:46 -0600

    Create theory task model

--- src/models/task.ts
+++ src/models/task.ts
@@ -7,6 +7,21 @@ class Task {
   lessonIndex: number;
   progress: number;
   lessonId: string;
+  type: 'theory' | 'practice';
 }
 
-export { Task };
+class TheoryTask extends Task {
+  category:
+    | 'grapheme_and_phoneme'
+    | 'grapheme_and_picture'
+    | 'phoneme_and_picture';
+  grapheme?: string;
+  phoneme?: string;
+  picture?: string;
+}
+
+class PracticeTask extends Task {
+  //
+}
+
+export { Task, TheoryTask, PracticeTask };
