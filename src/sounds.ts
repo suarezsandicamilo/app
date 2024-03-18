9a4d04a820e3a9cc0c742512e59e2dab0383715e
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-03-18 15:16:57 -0600

    Import the sounds as assets

---
+++ src/sounds.ts
@@ -0,0 +1,9 @@
+//
+
+export const sounds = {
+  'a.mp3': require('./../assets/sounds/a.mp3'),
+  'e.mp3': require('./../assets/sounds/e.mp3'),
+  'i.mp3': require('./../assets/sounds/i.mp3'),
+  'o.mp3': require('./../assets/sounds/o.mp3'),
+  'u.mp3': require('./../assets/sounds/u.mp3'),
+};
